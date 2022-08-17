8fcf2550ddf9ae05fb07ec0ceb2389f264bec17e
Luis Chang <luis.chang@iugo.com.uy>
2022-08-17 16:33:13 -0500

    add get login test

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ app.listen(5500, () => {
 //Sample GET request
 app.get('/test2', (req, res) => {
     res.send('Hello World');
+
+    //Insecure code example
+    const name = req.query.name;
+    const age = req.query.age;
+    console.log(name, age);
 });
 
 //Sample GET request with parameters and JSON
 app.get('/test', (req, res) => {
     res.send({ name: 'John', age: 25 });
 });
+
+app.get('/login', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    res.send({ user: username, pwd: password });
+});  
